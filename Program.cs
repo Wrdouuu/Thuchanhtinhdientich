227b85fbe3f8c5faf5b6a1915b1bc038bafa552e
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-05 21:30:18 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace THUCHANHTINHDIENTICH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            int width;
+            int area;
+
+            Console.WriteLine("Enter height: ");
+            height = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter width: ");
+            width = Int32.Parse(Console.ReadLine());
+
+            area = width * height;
+            Console.WriteLine("Area is: " + area);
+
+
+        }
+    }
+}
+
